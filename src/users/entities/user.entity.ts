972ab79f358f32a61ec2e5e5a45a78c1a3ef9c9d
farhadfahri <91671369+farhadfahri@users.noreply.github.com>
2022-08-16 18:55:12 +0300

    users crud added

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,17 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
 
-@ObjectType()
+@ObjectType('User')
 export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String)
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @Field(() => String)
+  email: string;
+
+  @Column()
+  @Field(() => Int)
+  roleId: number;
 }
